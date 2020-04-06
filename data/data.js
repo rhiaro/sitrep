843986318790b374283ee3c1eeeee45d7f26ce1e
Yancy Ribbens <yancy.ribbens@gmail.com>
2020-04-06 11:37:35 -0500

    add some locations

--- data/data.js
+++ data/data.js
@@ -15,10 +15,32 @@ var data = [
         "coordinates": [51.509865, -0.118092],
         "visits": [
         {
-            "content": "For some crypto thing",
+            "content": "Advancing Bitcoin and Optech workshop",
             "startDate": "2020-02-04T19:00:00+01:00",
-            "endDate": "2020-02-06T21:00:00+01:00"
+            "endDate": "2020-02-08T21:00:00+01:00"
         }
+        ]
+    },
+    {
+        "name": "Amsterdam",
+        "coordinates": [52.3667, 4.8945],
+        "visits": [
+        {
+            "content": "Visiting with the W3C (DID F2F)",
+            "startDate": "2020-01-28T09:00:00+01:00",
+            "endDate": "2020-02-04T22:00:00+01:00"
+        }
+        ]
+    },
+    {
+        "name": "Buxton, UK",
+        "coordinates": [53.2591, 1.9148],
+        "visits": [
+            {
+                "content": "Hiking in the Peak District",
+                "startDate": "2020-01-23T09:00:00+01:00",
+                "endDate": "2020-01-27T09:00:00+01:00"
+            }
         ]
     },
     {
@@ -28,7 +50,7 @@ var data = [
         {
             "content": "Staying out of Schengen for a while",
             "startDate": "2020-01-04T09:00:00+01:00",
-            "endDate": "2020-02-04T18:00:00+01:00"
+            "endDate": "2020-01-27T18:00:00+01:00"
         }
         ]
     },
@@ -37,10 +59,10 @@ var data = [
         "coordinates": [48.864716, 2.349014],
         "visits": [
         {
-            "content": "New Year's",
+            "content": "New Year's with Paul and friends",
             "startDate": "2019-12-28T09:00:00+01:00",
             "endDate": "2020-01-03T22:00:00+01:00"
         }
         ]
-    }
+    },
 ];
